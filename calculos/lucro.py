2f1ab6f197acba7c30f5cfda3683eecc66278bd8
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2021-03-16 22:39:47 -0300

    comit

--- calculos/lucro.py
+++ calculos/lucro.py
@@ -3,6 +3,6 @@ from .sraping_yahoo import *
 
 def calc_lucro(acao):
     ticket = list(acao.keys())[0]
-    x = info_das_acoes(ticket,nacional=acao[list(acao.keys())[0]]['nacional'])
-    print(x['info'])
-    return 'ok'
+    fechamento = float(info_das_acoes(ticket,nacional=acao[list(acao.keys())[0]]['nacional'])['info'][0]['dados']['close'])
+    lucro = (fechamento * float(acao[ticket]['qtd'])) - float(acao[ticket]['pos'])
+    return lucro
