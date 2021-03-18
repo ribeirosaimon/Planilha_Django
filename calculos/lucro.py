a9bb86329e765ad2116ccc7fc25fbbce13fe2b18
Saimon <saimonribeiros@hotmail.com>
2021-03-18 16:38:46 -0300

    bug

--- calculos/lucro.py
+++ calculos/lucro.py
@@ -4,21 +4,12 @@ from .get_dolar_price import *
 def calc_lucro(acao, info_das_acoes):
     preco = 0
     ticket = list(acao.keys())[0]
-<<<<<<< HEAD
     for x in info_das_acoes:
         if x['acao'] == ticket:
             preco = x['info'][0]['dados']['close']
     quantidade = acao[list((acao.keys()))[0]]['qtd']
     posicao = acao[list((acao.keys()))[0]]['pos']
-
     lucro = (preco * quantidade) - float(posicao)
     if acao[list((acao.keys()))[0]]['nacional'] == False:
         lucro *= get_dolar_price()
     return round(lucro, 2)
-
-
-=======
-    fechamento = float(info_das_acoes(ticket,nacional=acao[list(acao.keys())[0]]['nacional'])['info'][0]['dados']['close'])
-    lucro = (fechamento * float(acao[ticket]['qtd'])) - float(acao[ticket]['pos'])
-    return lucro
->>>>>>> 2f1ab6f197acba7c30f5cfda3683eecc66278bd8
