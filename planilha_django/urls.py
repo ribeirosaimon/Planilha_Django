c913740f50b696fd4feaddd6c29687a31bd34172
Saimon <saimonribeiros@hotmail.com>
2021-03-25 16:14:29 -0300

    Update urls.py

--- planilha_django/urls.py
+++ planilha_django/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from carteira.urls import CarteiraRouter
 from compras.urls import ComprasRouter
 from vendas.urls import VendasRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api/v1/', include(ComprasRouter.urls)),
@@ -10,7 +11,8 @@ urlpatterns = [
     path('api/v1/', include(CarteiraRouter.urls)),
     
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
+    path('token/', obtain_auth_token, name='api_token_auth')
 ]
 
 
