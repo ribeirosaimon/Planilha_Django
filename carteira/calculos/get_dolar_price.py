bee0e747a387568fe3353094382bd104652fb9d6
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2021-04-23 22:27:44 -0300

    Bug Dolar

--- carteira/calculos/get_dolar_price.py
+++ carteira/calculos/get_dolar_price.py
@@ -5,7 +5,7 @@ def get_dolar_price():
     return settings.DOLAR_TODAY
 
 def preco_dolar():
-    endpoint  = 'https://economia.awesomeapi.com.br/json/daily/USD-BRL/2'
+    endpoint  = 'https://economia.awesomeapi.com.br/last/USD-BRL'
     resposta = requests.request('GET', endpoint)
-    dolar_do_dia =  float(resposta.json()[0]['ask'])
+    dolar_do_dia =  float(resposta.json()['USDBRL']['ask'])
     return dolar_do_dia
